edec86b8ed57337a3850896399e14aabae315d03
Valerie Campbell <valeriecs10@gmail.com>
2020-07-09 16:15:55 -0700

    removed NullCard class, refactored Hand to use empty array instead of NullCards

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,11 +1,10 @@
 require_relative 'card'
-require_relative 'null_card'
 
 class Hand
     attr_accessor :cards
 
     def initialize
-        @cards = Array.new(5) { NullCard.instance }
+        @cards = Array.new
     end
 
     def [](i)
