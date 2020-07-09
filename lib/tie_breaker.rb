71db57e36e1f992f402b6f64a581e8a577b89f0d
Valerie Campbell <valeriecs10@gmail.com>
2020-07-08 20:04:20 -0700

    almost finished TieBreaker class. Needs to be tested.

---
+++ lib/tie_breaker.rb
@@ -0,0 +1,59 @@
+class TieBreaker
+    def self.break_tie(tied_players)
+        tie_breaking_values = []
+        tied_players.each do |player|
+            tie_breaking_values << [tie_breaking_value(player), player]
+        end
+        tie_breaking_values.sort[0][1]
+    end
+
+    def self.tie_breaking_value(player)
+        case PokerHands.best_hand(player.hand)
+        when :royal_flush || :straight_flush || :full_house || 
+                :flush || :straight || :high_card
+            high_card(player.hand)
+        when :four_of_a_kind
+            highest_of_set(player.hand, 4)
+        when :three_of_a_kind
+            highest_of_set(player.hand, 3)
+        when :two_pair
+            # highest of the pairs
+        when :one_pair
+            highest_of_set(player.hand, 2)
+        end
+    end
+
+    def high_card(hand)
+        values = []
+        hand.each do |card|
+            values << card.value
+        end
+        values.sort[0]
+    end
+
+    def self.highest_of_set(hand, num)
+        value_tallies(hand).each do |val, total|
+            return val if total == num
+        end
+    end
+
+    def self.highest_of_two_pair(hand)
+        hand_dup = hand.dup
+        value_tallies(hand).each do |val, total|
+            if total == 1
+                hand_dup.cards.each do |card|
+                    hand_dup.delete(card) if card.value == val
+                end
+            end
+        end
+        high_card(hand_dup)
+    end
+
+    def self.tally_cards(hand)
+        value_tallies = Hash.new(0)
+        hand.cards.each do |card|
+            value_tallies[card.value] += 1
+        end
+        value_tallies
+    end
+end
