1b4b5cc6ded2168a0ac9cf75b61e8e2d8a23aa18
Valerie Campbell <valeriecs10@gmail.com>
2020-07-09 16:50:00 -0700

    finished player Class and testing

---
+++ lib/player.rb
@@ -0,0 +1,50 @@
+require_relative 'hand'
+
+class Player
+    attr_reader :hand, :pot
+
+    def initialize(pot)
+        @hand = Hand.new
+        @pot = pot
+    end
+
+    def cards_to_discard
+        user_input = get_user_cards
+        return 0 if user_input[0] == 0
+        card_indexes = user_input.map { |i| i -= 1 }
+        card_indexes.sort!.reverse!
+        card_indexes.each do |i|
+            hand.cards.delete_at(i)
+        end
+        card_indexes.length
+    end
+
+    def fold_see_raise
+        user_bet = get_user_bet
+        case user_bet
+        when :fold
+            false
+        when :see
+            true
+        when :raise
+            get_user_raise
+        end
+    end
+
+    private
+
+    def get_user_cards
+        puts "Enter the numbers of the cards you'd like to exchange, or 0 for none."
+        gets.chomp.split.map { |n| n.to_i }
+    end
+
+    def get_user_bet
+        puts "Would you like to fold, see or raise?"
+        gets.chomp.downcase.to_sym
+    end
+
+    def get_user_raise
+        puts "How much would you like to raise?"
+        gets.chomp.to_i
+    end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,2 +1,85 @@
 require 'rspec'
 require 'player'
+
+describe 'Player' do
+    subject(:player) { Player.new(20) }
+
+    describe '#hand' do
+        it 'is a Hand' do
+            expect(player.hand).to be_a(Hand)
+        end
+    end
+
+    describe '#pot' do
+        it 'is an Integer' do
+            expect(player.pot).to be_an(Integer)
+        end
+
+        it 'starts at given amount' do
+            expect(player.pot).to eq(20)
+        end
+    end
+        
+    describe '#cards_to_discard' do
+        let(:card1) { double("card1") }
+        let(:card2) { double("card1") }
+        let(:card3) { double("card1") }
+        let(:card4) { double("card1") }
+        let(:card5) { double("card1") }
+        
+        before(:each) { player.hand.cards = [card1, card2, card3, card4, card5] }
+
+        it 'calls #get_user_cards' do
+            allow(player).to receive(:get_user_cards).and_return(0)
+            expect(player).to receive(:get_user_cards)
+            player.cards_to_discard
+        end
+
+        context 'no cards discarded' do
+            it 'does not change hand' do
+                allow(player).to receive(:get_user_cards).and_return([0])
+                player.cards_to_discard
+                expect(player.hand.cards.length).to eq(5)
+            end
+        end
+        
+        context 'cards discarded' do
+            it 'removes correct number of cards from hand' do
+                allow(player).to receive(:get_user_cards).and_return([2, 3])
+                player.cards_to_discard
+                expect(player.hand.cards.length).to eq(3)
+            end
+        end
+    end
+    
+    describe '#fold_see_raise' do
+        subject(:player) { Player.new(20) }
+
+        context 'user wants to fold' do
+            it 'returns false' do
+                allow(player).to receive(:get_user_bet).and_return(:fold)
+                expect(player.fold_see_raise).to eq(false)
+            end
+        end
+
+        context 'user wants to see' do
+            it 'returns true' do
+                allow(player).to receive(:get_user_bet).and_return(:see)
+                expect(player.fold_see_raise).to eq(true)
+            end
+        end
+
+        context 'user wants to raise' do
+            it 'calls #get_user_raise and returns an integer' do
+                allow(player).to receive(:get_user_bet).and_return(:raise)
+                allow(player).to receive(:get_user_raise).and_return(2)
+                player.fold_see_raise
+                
+                expect(player).to receive(:get_user_raise)
+                expect(player.fold_see_raise).to be_an(Integer)
+            end
+        end
+
+    end
+
+end
