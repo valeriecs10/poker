17081381e81f461954468e938e8828853f9ff9c4
Valerie Campbell <valeriecs10@gmail.com>
2020-07-09 16:19:37 -0700

    adjusted for changes to Hand after deleting NullClass

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -5,17 +5,8 @@ describe 'Hand' do
     subject(:hand) { Hand.new }
 
         describe '#cards' do
-            it 'is an array' do
-                expect(hand.cards).to be_an(Array)
-            end
-            
-            it 'has 5 elements' do
-                expect(hand.cards.length).to eq(5)
-            end
-
-            it 'contains all cards' do
-                expect(hand.cards).to all(be_a(Card))
+            it 'is an empty array' do
+                expect(hand.cards).to be_empty
             end
         end
-
 end
