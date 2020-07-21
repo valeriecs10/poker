61050c9876b03ea991dc1831542bb340ba119a88
Valerie Campbell <valeriecs10@gmail.com>
2020-07-20 18:59:18 -0700

    added to_s method

--- lib/card.rb
+++ lib/card.rb
@@ -5,4 +5,38 @@ class Card
         @value = value
         @suit = suit
     end
+
+    def to_s
+        p "| #{printed_val} #{printed_suit} |"
+    end
+
+    private
+
+    def printed_val
+        case value
+        when 1
+            'A'
+        when 2..10
+            value.to_s
+        when 11
+            'J'
+        when 12
+            'Q'
+        when 13
+            'K'
+        end
+    end
+
+    def printed_suit
+        case suit
+        when :heart
+            '♥'
+        when :spade
+            '♠'
+        when :diamond
+            '♦'
+        when :club
+            '♣'
+        end
+    end
 end
