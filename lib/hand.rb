084babc2a5960b0173e8fc30f9b433892012a97e
Valerie Campbell <valeriecs10@gmail.com>
2020-07-22 20:00:51 -0700

    added to_s method

--- lib/hand.rb
+++ lib/hand.rb
@@ -18,4 +18,11 @@ class Hand
     def num_cards
         @cards.length
     end
+
+    def to_s
+        cards.each { |card| print card }
+        puts
+        cards.each_with_index { |card, i| print "   #{i + 1}    " }
+        puts
+    end
 end
